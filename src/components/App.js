8d999ee086309e1c5a0608fd2b970f14229b8fd2
iona-b <ionabrabender@gmail.com>
2020-08-08 17:56:59 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,36 @@ class App extends React.Component {
     }
   }
 
+  onFindPetsClick = () => {
+    const type = this.state.filters.type === "all" ? "" : "?type=" + this.state.filters.type
+    const url = `/api/pets${type}`
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((pets) => {
+      this.setState({
+        pets : pets,
+      });
+    });
+  };
+
+  onChangeType = (e) => {
+    const petType = e.target.value
+    this.setState({
+      filters: {
+        type: petType
+      }
+    });
+  }
+
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet;
+    });
+    this.setState ({
+      pets
+    });
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +54,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
